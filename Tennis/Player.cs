f8016d00adb8bd515fb2e41e28dcd2747db154fd
Emre Rauhofer <emre.rauhofer.extern@orfnet.at>
2023-10-13 11:06:09 +0200

    added class for player

---
+++ Tennis/Player.cs
@@ -0,0 +1,14 @@
+﻿namespace Tennis
+{
+    public class Player
+    {
+        private string _playerName;
+        private int _playerPoints;
+
+        public Player(string playerName, int playerPoints)
+        {
+            _playerName = playerName;
+            _playerPoints = playerPoints;
+        }
+    }
+}
