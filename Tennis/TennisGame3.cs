1a75a7ba40c280f7c06a22a5561a35676d77548d
Emre Rauhofer <emre.rauhofer.extern@orfnet.at>
2023-10-13 11:09:57 +0200

    renamed player instance

--- Tennis/TennisGame3.cs
+++ Tennis/TennisGame3.cs
@@ -6,13 +6,13 @@ namespace Tennis
     {
         private int _player2points;
         private int _player1points;
-        private IPlayer _player1Name;
+        private IPlayer _player1;
         private string _player2Name;
         private string[] tennisTerms = { "Love", "Fifteen", "Thirty", "Forty" };
 
         public TennisGame3(string player1Name, string player2Name)
         {
-            this._player1Name = new Player(player1Name, 0);
+            this._player1 = new Player(player1Name, 0);
             this._player2Name = player2Name;
         }
 
@@ -31,13 +31,13 @@ namespace Tennis
                 return "Deuce";
             }
                 
-            string winningPlayerName = _player1points > _player2points ? _player1Name.PlayerName : _player2Name;
+            string winningPlayerName = _player1points > _player2points ? _player1.PlayerName : _player2Name;
             return (Math.Abs(_player1points-_player2points) == 1) ? $"Advantage {winningPlayerName}" : $"Win for {winningPlayerName}";
         }
 
         public void WonPoint(string playerName)
         {
-            if (playerName == this._player1Name.PlayerName)
+            if (playerName == this._player1.PlayerName)
             {
                 this._player1points += 1;
             }
